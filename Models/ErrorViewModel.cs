37b9cbadfab5f0ae8a1055ec795959fb8f54f868
cdrobpaz <cdrobpaz@gmail.com>
2023-01-27 18:41:49 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcSeriesPersonajesCRP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
